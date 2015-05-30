3d893e6c2ca0600250fcbefabd8d7513f9974686
RummeniggeMaia <rummeniggemaia@gmail.com>
2015-05-30 01:09:39 -0300

    ajustes no cadastro de funcionairo

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 
-require_once 'bootstrap.php';
+require_once "vendor/autoload.php";
 require_once './vendor/twig/twig/lib/Twig/Autoloader.php';
 
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem('web/');
+$loader = new Twig_Loader_Filesystem('src/visao/');
 $twig = new Twig_Environment($loader);
 
 $template = $twig->loadTemplate('home.twig');
-echo $template->render(array());
+echo $template->render(
+        array(
+            'semantic_js' => 'web/semantic_ui/semantic.min.js',
+            'semantic_css' => 'web/semantic_ui/semantic.min.css',
+            'jquerylib' => 'web/jquery.min.js',
+            'front_ctrl' => 'src/controle/front_controller.php',
+            'logo' => 'web/imagens/icon-process-white.png')
+);
--- src/controle/front_controller.php
+++ src/controle/front_controller.php
@@ -1,3 +1,16 @@
 <?php
 
-print var_dump($_POST);
+require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/autoload.php");
+require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/twig/twig/lib/Twig/Autoloader.php");
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem("$_SERVER[DOCUMENT_ROOT]/sgp/src/visao");
+$twig = new Twig_Environment($loader);
+
+if (isset($_POST['navegador_funcionario'])) {
+    $template = $twig->loadTemplate('/forms/form_funcionario.twig');
+    echo $template->render(array());
+    return;
+}
+print "Navegação incorreta";

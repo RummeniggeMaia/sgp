e741e574df754e86ef1e62db273bdc1877b16691
RummeniggeMaia <rummeniggemaia@gmail.com>
2015-05-31 15:50:40 -0300

    navegacao das paginas de gerenciamento de funcionarios, departamentos e assuntos

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 require_once "vendor/autoload.php";
 require_once './vendor/twig/twig/lib/Twig/Autoloader.php';
 
+session_start();
+
+$visoes_navegacao = array(
+    'visao_gerenciar_funcionario' => 'forms/form_funcionario.twig',
+    'visao_gerenciar_assunto' => 'forms/form_assunto.twig',
+    'visao_gerenciar_departamento' => 'forms/form_departamento.twig',
+    'visao_home' => 'home.twig'
+);
+
+$_SESSION['visoes_navegacao'] = serialize($visoes_navegacao);
+
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('src/visao/');
 $twig = new Twig_Environment($loader);
 
-$template = $twig->loadTemplate('home.twig');
+$template = $twig->loadTemplate($visoes_navegacao['visao_home']);
 echo $template->render(
         array(
             'semantic_js' => 'web/semantic_ui/semantic.min.js',
--- src/controle/front_controller.php
+++ src/controle/front_controller.php
@@ -2,15 +2,28 @@
 
 require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/autoload.php");
 require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/twig/twig/lib/Twig/Autoloader.php");
+require_once("$_SERVER[DOCUMENT_ROOT]/sgp/src/util/Util.php");
+
+session_start();
+
+$visoes_navegacao = unserialize($_SESSION["visoes_navegacao"]);
 
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem("$_SERVER[DOCUMENT_ROOT]/sgp/src/visao");
 $twig = new Twig_Environment($loader);
 
-if (isset($_POST['navegador_funcionario'])) {
-    $template = $twig->loadTemplate('/forms/form_funcionario.twig');
-    echo $template->render(array());
-    return;
+foreach ($_POST as $requisicao) {
+    if (is_string($requisicao)) {
+        if (Util::starsWithString($requisicao, "navegador_")) {
+            $visao = str_replace("navegador_", "", $requisicao);
+            if (isset($visoes_navegacao[$visao])) {
+                $template = $twig->loadTemplate($visoes_navegacao[$visao]);
+                print $template->render(array());
+                return;
+            }
+        }
+    }
 }
 print "Navegação incorreta";
+return;

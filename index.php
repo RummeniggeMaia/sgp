5f152f6d422a2542572cd6ba0cd22e46260b7d56
RummeniggeMaia <rummeniggemaia@gmail.com>
2015-06-01 16:22:43 -0300

    Adicionado os comentrios basicos da navegacao do sistema

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 require_once "vendor/autoload.php";
 require_once './vendor/twig/twig/lib/Twig/Autoloader.php';
 
+//Função utilizada pra iniciar a sessão em scripts no php
 session_start();
 
+//Nesse vetor sao colocas as páginas de navegacao do sitema
 $visoes_navegacao = array(
     'visao_gerenciar_funcionario' => 'forms/form_funcionario.twig',
     'visao_gerenciar_assunto' => 'forms/form_assunto.twig',
@@ -12,15 +14,21 @@ $visoes_navegacao = array(
     'visao_home' => 'home.twig'
 );
 
+//Pra inserir qualquer objeto na sessao é necessario serializa-lo, 
+//visoes_navegacao é a chave de mapeamento pra esse vetor, as variaveis de
+//sessao sao acessadas de todos os locais do sitema
 $_SESSION['visoes_navegacao'] = serialize($visoes_navegacao);
-
+//Inicia o twig
 Twig_Autoloader::register();
-
+//Carrega o diretorio dos templates, aqui é src/visao por causa do local do
+//index.php
 $loader = new Twig_Loader_Filesystem('src/visao/');
 $twig = new Twig_Environment($loader);
-
+//Gera a interface home de acordo com o template home.twig
 $template = $twig->loadTemplate($visoes_navegacao['visao_home']);
-echo $template->render(
+//Renderiza a pagina home de acordo com o template. O vetor serve para passar
+//os diretorios dos aquivos web.
+print $template->render(
         array(
             'semantic_js' => 'web/semantic_ui/semantic.min.js',
             'semantic_css' => 'web/semantic_ui/semantic.min.css',
--- src/controle/front_controller.php
+++ src/controle/front_controller.php
@@ -1,23 +1,30 @@
 <?php
-
+//Este script é utilizado para fazer a navegacao do 
 require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/autoload.php");
 require_once("$_SERVER[DOCUMENT_ROOT]/sgp/vendor/twig/twig/lib/Twig/Autoloader.php");
 require_once("$_SERVER[DOCUMENT_ROOT]/sgp/src/util/Util.php");
-
+//Inicia a sessao novamente
 session_start();
-
+//Acessa o vetor de navegacao criado no index.php
 $visoes_navegacao = unserialize($_SESSION["visoes_navegacao"]);
 
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem("$_SERVER[DOCUMENT_ROOT]/sgp/src/visao");
 $twig = new Twig_Environment($loader);
-
+//Para cada requisicao feita a esse script, é necessario saber se trata de um 
+//link ou um comando de controle
 foreach ($_POST as $requisicao) {
     if (is_string($requisicao)) {
+        //Os link do sistema tem que começar com 'navegador_' seguido da visao 
+        // a ser acessada
         if (Util::starsWithString($requisicao, "navegador_")) {
+            //remove a palavra navegador_
             $visao = str_replace("navegador_", "", $requisicao);
+            //Verifica se essa visao existe no sistema, nesse caso no vetor
+            //de visões
             if (isset($visoes_navegacao[$visao])) {
+                //Gera o template e manda renderizar a visao .twig
                 $template = $twig->loadTemplate($visoes_navegacao[$visao]);
                 print $template->render(array());
                 return;

4cae43a79e19abeccaaea5abceb1aabe043abdb4
Jaedson Araujo <jaedsonbruno@gmail.com>
2015-07-08 23:45:02 -0300

    Validacao dos campos funcionando

--- src/controle/FuncionarioCtrl.php
+++ src/controle/FuncionarioCtrl.php
@@ -89,14 +89,12 @@ class FuncionarioCtrl implements Controlador {
 
     public function executarFuncao($post, $funcao) {
         $this->gerarFuncionario($post);
-        
-       if($funcao == "cadastrar"){
-           $resultado = $this->validacao();           
-           if($resultado == "validacao_erro") return 'gerenciar_funcionario';
-       }
-       
-       
-       if ($funcao == "cadastrar") {
+        if ($funcao == "cadastrar") {
+            $resultado = $this->validarCampos();
+            if ($resultado == "validacao_erro")
+                return 'gerenciar_funcionario';
+        }
+        if ($funcao == "cadastrar") {
             $this->dao->criar($this->funcionario);
             $this->funcionario = new Funcionario("", "", "");
             $this->mensagem = new Mensagem(
@@ -127,31 +125,63 @@ class FuncionarioCtrl implements Controlador {
         $this->modeloTabela->setLinhas($linhas);
     }
 
-    /*Falta resolver o problema da mascara, pois a mascara tem q ser tirada antes da verificação, para conferir se
-     o CPF e RG é válido e são numéricos realmente.*/
-    private function validacao() {
-        if ($this->funcionario->getNome() == null || is_numeric($this->funcionario->getNome())) {
+    /* Falta resolver o problema da mascara, pois a mascara tem q ser tirada antes da verificação, para conferir se
+      o CPF e RG é válido e são numéricos realmente. */
+
+    private function validarCampos() {
+
+        if ($this->funcionario->getNome() == null) {
             $this->mensagem = new Mensagem(
                     "Cadastro não realizado!"
                     , "msg_tipo_erro"
-                    , "O campo nome não pode ser vazio ou numérico");
+                    , "O campo nome não pode ser vazio");
             return 'validacao_erro';
         }
-        if ($this->funcionario->getRg() == null || is_numeric($this->funcionario->getRg())) {
+        if ($this->funcionario->getRg() == null) {
             $this->mensagem = new Mensagem(
                     "Cadastro não realizado!"
                     , "msg_tipo_erro"
                     , "O campo RG não pode ser vazio");
             return 'validacao_erro';
         }
-        if ($this->funcionario->getCpf() == null || is_numeric($this->funcionario->getCpf())) {
+        if ($this->funcionario->getCpf() == null) {
             $this->mensagem = new Mensagem(
                     "Cadastro não realizado!"
                     , "msg_tipo_erro"
                     , "O campo CPF não pode ser vazio");
             return 'validacao_erro';
-        }         
-        
+        }
+        if (!($this->validarCPF($this->funcionario->getCpf()))) {
+            $this->mensagem = new Mensagem(
+                    "Cadastro não realizado!"
+                    , "msg_tipo_erro"
+                    , "CPF inválido!");
+            return 'validacao_erro';
+        }
+    }
+
+    private function validarCPF($cpf) { { // Verifiva se o número digitado contém todos os digitos
+            $cpf = str_pad(ereg_replace('[^0-9]', '', $cpf), 11, '0', STR_PAD_LEFT);
+
+            // Verifica se nenhuma das sequências abaixo foi digitada, caso seja, retorna falso
+            if (strlen($cpf) != 11 || $cpf == '00000000000' || $cpf == '11111111111' || $cpf == '22222222222' || $cpf == '33333333333' || $cpf == '44444444444' || $cpf == '55555555555' || $cpf == '66666666666' || $cpf == '77777777777' || $cpf == '88888888888' || $cpf == '99999999999') {
+                return false;
+            } else {   // Calcula os números para verificar se o CPF é verdadeiro
+                for ($t = 9; $t < 11; $t++) {
+                    for ($d = 0, $c = 0; $c < $t; $c++) {
+                        $d += $cpf{$c} * (($t + 1) - $c);
+                    }
+
+                    $d = ((10 * $d) % 11) % 10;
+
+                    if ($cpf{$c} != $d) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
     }
 
 }
